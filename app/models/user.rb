87fee113e775c25f2f1326881703ecfa08abe3d4
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2020-10-04 00:00:49 +0300

    added password encryption and method of authenticate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,50 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
   has_many :questions
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
+  validates :password, presence: true, on: :create
+
+  attr_accessor :password
+
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+        )
+      )
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    return nil unless user.present?
+
+    hashed_password = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(
+        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+      )
+    )
+
+    return nil unless hashed_password == user.password_hash
+
+    user
+  end
 end
