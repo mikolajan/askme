619cd0f5b95e726dff276e999fba846f24e28e35
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2020-10-11 16:06:43 +0300

    added module sklonenie, helper user_questions_count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
+require 'sklonenie'
+
 module ApplicationHelper
   def user_avatar(user)
-    user.avatar_url || asset_path 'no_avatar.jpg'
+    user.avatar_url || asset_path('no_avatar.jpg')
+  end
+
+  def user_questions_count(number_of_question, text_if_null, text_if_not_null)
+    number_of_question.zero? ? text_if_null : text_if_not_null + ' ' +
+    sklonenie(number_of_question, 'вопрос', 'вопроса', 'вопросов', number_of_question)
   end
 end
---
+++ lib/sklonenie.rb
@@ -0,0 +1,17 @@
+module Sklonenie
+  def self.sklonenie(number, one, few, many, with_number = false)
+    prefix = with_number ? "#{number} " : ''
+
+    return prefix + many if (number % 100).between?(11, 14)
+
+    word =
+      case number % 10
+      when 1 then one
+      when 2..4 then few
+      else
+        many
+      end
+
+    prefix + word
+  end
+end
