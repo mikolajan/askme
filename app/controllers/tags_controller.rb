111a10076d0570ec2afa109c93ccb6ac90fbd650
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2020-11-09 20:25:37 +0300

    midified tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,7 @@
 class TagsController < ApplicationController
-  before_action :load_tag
-
   def show
+    @tag = Tag.with_questions.find_by!(name: params[:name])
+
     @questions = @tag.questions
-  end
-
-  private
-
-  def load_tag
-    @tag = Tag.find_by!(name: params[:name])
   end
 end
